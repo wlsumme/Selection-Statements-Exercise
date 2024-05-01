e8a3e6b66306bf799cd174972ce334f38df2045a
William Summe <wlsumme@gmail.com>
2024-05-01 11:06:47 -0500

    Guessed it

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+            Console.WriteLine("Guess a number between 1 and 10");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
--- SelectionStatementExercise/obj/Debug/net6.0/SelectionStatementExercise.AssemblyInfo.cs
+++ SelectionStatementExercise/obj/Debug/net6.0/SelectionStatementExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc20364378e57b598a57e66f3297e88f8b323b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
